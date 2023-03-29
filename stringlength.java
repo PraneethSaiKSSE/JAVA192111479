151559213a7a0705d9c88a6d54755f7e1509962b
PraneethSaiKSSE <113979088+PraneethSaiKSSE@users.noreply.github.com>
2023-03-29 23:44:33 +0530

    Add files via upload

---
+++ stringlength.java
@@ -0,0 +1,10 @@
+public class stringlength {
+    public static void main(String[] args)
+      {  
+        String str = "praneeth";
+        
+        int len = str.length();
+        System.out.println("The string length of '"+str+"' is: "+len);
+       }
+    }
+    
