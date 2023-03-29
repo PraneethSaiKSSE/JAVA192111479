e93fae14383f07f433db9883cd66553c3c5922f3
PraneethSaiKSSE <113979088+PraneethSaiKSSE@users.noreply.github.com>
2023-03-29 23:41:31 +0530

    Add files via upload

---
+++ praneeth2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+public class praneeth2
+{
+	
+   public static void printStars(int n)
+	{
+		int i, j;
+
+	
+		
+		for(i=0; i<n; i++)
+		{
+
+			
+			
+			for(j=0; j<=i; j++)
+			{
+				
+				System.out.print("% ");
+			}
+
+			
+			System.out.println();
+		}
+}
+
+	
+	public static void main(String args[])
+	{
+		int n = 3;
+		printStars(n);
+	}
+}
