586507b17fa75535365405bb34de46cd742263c2
PraneethSaiKSSE <113979088+PraneethSaiKSSE@users.noreply.github.com>
2023-03-29 23:39:30 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Pattern3
+{
+    public static void main(String[] args)
+    {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("Enter the number of rows to print the pattern ");
+
+        int rows = scanner.nextInt();
+        int k = 1;
+        
+        System.out.println("** Printing the pattern... **");
+        
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(k*k + " ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
